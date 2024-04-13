69f78e5bb8c09c9c4879b2016154f253ac3b906d
aksza <suciu.aksza@student.ms.sapientia.ro>
2024-04-14 01:00:04 +0300

    added restaurantcontroller

---
+++ DineEaseApp/Controllers/RestaurantController.cs
@@ -0,0 +1,32 @@
+﻿using DineEaseApp.Interfaces;
+using DineEaseApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DineEaseApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RestaurantController : Controller
+    {
+        private readonly IRestaurantRepository _restaurantRepository;
+
+        public RestaurantController(IRestaurantRepository restaurantRepository) 
+        {
+            _restaurantRepository = restaurantRepository;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(200,Type = typeof(IEnumerable<Restaurant>))]
+
+        public IActionResult GetRestaurants()
+        {
+            var restaurants = _restaurantRepository.GetRestaurants();
+
+            if(!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            return Ok(restaurants);
+        }
+    }
+}
---
+++ DineEaseApp/Repository/RestaurantRepository.cs
@@ -0,0 +1,20 @@
+﻿using DineEaseApp.Data;
+using DineEaseApp.Interfaces;
+using DineEaseApp.Models;
+
+namespace DineEaseApp.Repository
+{
+    public class RestaurantRepository : IRestaurantRepository
+    {
+        private readonly DataContext _context;
+        public RestaurantRepository(DataContext context) 
+        { 
+            _context = context;
+        }
+
+        public ICollection<Restaurant> GetRestaurants()
+        {
+            return _context.Restaurants.OrderBy(x => x.Id).ToList();
+        }
+    }
+}
