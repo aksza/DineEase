913c6e1f294d8e1367ae4644d32d2d3795384db2
aksza <suciu.aksza@student.ms.sapientia.ro>
2024-04-19 20:55:24 +0300

    modified restaurant and order table
    added password to restaurant model, and modified order comment field to nullable

---
+++ DineEaseApp/Migrations/20240419175102_FourthUpdate.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DineEaseApp.Migrations
+{
+    public partial class FourthUpdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "PasswordHash",
+                table: "Restaurants",
+                type: "varbinary(max)",
+                nullable: false,
+                defaultValue: new byte[0]);
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "PasswordSalt",
+                table: "Restaurants",
+                type: "varbinary(max)",
+                nullable: false,
+                defaultValue: new byte[0]);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Comment",
+                table: "Orders",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PasswordHash",
+                table: "Restaurants");
+
+            migrationBuilder.DropColumn(
+                name: "PasswordSalt",
+                table: "Restaurants");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Comment",
+                table: "Orders",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
